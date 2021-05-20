36390ea7b0e77fac3099265af34445187429a6d4
Survivor94 <16431089+Survivor94@users.noreply.github.com>
2021-05-20 11:50:18 +0200

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,2 +1,2 @@
-token = '1563494829:AAF_9dvX4A3ge2FPFH334NzEJz9pZZtYSms' # Create bot from BotFather https://telegram.me/botfather
-chatID = '1609558283' # Get from IDBot https://telegram.me/myidbot
+token = 'Get code from given link' # Create bot from BotFather https://telegram.me/botfather
+chatID = 'Get code from given link' # Get from IDBot https://telegram.me/myidbot
